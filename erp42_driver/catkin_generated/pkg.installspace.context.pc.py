6bee85a877ac9825c199b1d15698ab1510d68828
wkdev <chanhyukman@gmail.com>
2023-03-28 21:56:24 +0900

    removed

--- erp42_driver/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;geometry_msgs;nav_msgs;sensor_msgs;tf;erp42_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "erp42_driver"
-PROJECT_SPACE_DIR = "/home/parallels/erp42_melodic_ws/install"
-PROJECT_VERSION = "0.0.0"
